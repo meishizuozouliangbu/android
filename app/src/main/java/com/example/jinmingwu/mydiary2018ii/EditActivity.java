4b9764b71f2d5ce5d652f5c49c078713c8160856
jinmingwu <qqschool@126.com>
2018-06-21 20:54:10 +0800

    season 02 fix bug

--- app/src/main/java/com/example/jinmingwu/mydiary2018ii/EditActivity.java
+++ app/src/main/java/com/example/jinmingwu/mydiary2018ii/EditActivity.java
@@ -1,22 +1,11 @@
 package com.example.jinmingwu.mydiary2018ii;
 
-import android.Manifest;
-import android.content.DialogInterface;
-import android.os.Bundle;
 import android.app.AlertDialog;
-import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-import android.location.Criteria;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
 import android.os.Bundle;
-import android.os.HandlerThread;
-import android.os.Looper;
-import android.support.v4.widget.SimpleCursorAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
@@ -28,13 +17,6 @@ import android.widget.Toast;
 
 import com.baidu.location.LocationClient;
 import com.baidu.location.LocationClientOption;
-import com.tencent.map.geolocation.TencentLocation;
-import com.tencent.map.geolocation.TencentLocationListener;
-import com.tencent.map.geolocation.TencentLocationManager;
-import com.tencent.map.geolocation.TencentLocationRequest;
-import android.app.AlertDialog.Builder;
-
-import org.w3c.dom.Text;
 
 import java.text.DateFormat;
 import java.util.Date;
@@ -79,45 +61,6 @@ public class EditActivity extends AppCompatActivity {
 
         //--------------------------------配置定位BAIDU SDK 参数 开始
         LocationClientOption option = new LocationClientOption();
-
-        option.setLocationMode(LocationClientOption.LocationMode.Hight_Accuracy);
-        //可选，设置定位模式，默认高精度
-        //LocationMode.Hight_Accuracy：高精度；
-        //LocationMode. Battery_Saving：低功耗；
-        //LocationMode. Device_Sensors：仅使用设备；
-
-        option.setCoorType("bd09ll");
-        //可选，设置返回经纬度坐标类型，默认gcj02
-        //gcj02：国测局坐标；
-        //bd09ll：百度经纬度坐标；
-        //bd09：百度墨卡托坐标；
-        //海外地区定位，无需设置坐标类型，统一返回wgs84类型坐标
-
-        option.setScanSpan(0);
-        //可选，设置发起定位请求的间隔，int类型，单位ms
-        //如果设置为0，则代表单次定位，即仅定位一次，默认为0
-        //如果设置非0，需设置1000ms以上才有效
-
-        option.setOpenGps(true);
-        //可选，设置是否使用gps，默认false
-        //使用高精度和仅用设备两种定位模式的，参数必须设置为true
-
-        option.setLocationNotify(false);
-        //可选，设置是否当GPS有效时按照1S/1次频率输出GPS结果，默认false
-
-        option.setIgnoreKillProcess(false);
-        //可选，定位SDK内部是一个service，并放到了独立进程。
-        //设置是否在stop的时候杀死这个进程，默认（建议）不杀死，即setIgnoreKillProcess(true)
-
-        option.SetIgnoreCacheException(false);
-        //可选，设置是否收集Crash信息，默认收集，即参数为false
-
-        option.setWifiCacheTimeOut(5*60*1000);
-        //可选，7.2版本新增能力
-        //如果设置了该接口，首次启动定位时，会先判断当前WiFi是否超出有效期，若超出有效期，会先重新扫描WiFi，然后定位
-
-        option.setEnableSimulateGps(false);
-        //可选，设置是否需要过滤GPS仿真结果，默认需要，即参数为false
 
         option.setIsNeedAddress(true);
         //可选，是否需要地址信息，默认为不需要，即参数为false
@@ -227,7 +170,6 @@ public class EditActivity extends AppCompatActivity {
                 }
                 break;
             case R.id.edit_map: //获取地图
-                //startActivity(new Intent(this,TencentActivity.class));
                 //findMap();
                 String address = myListener.getAddr();
                 myLocationText.setText(address);
@@ -302,7 +244,7 @@ public class EditActivity extends AppCompatActivity {
         getNoteDao().insert(note);
         Log.d(TAG, "Inserted new note, ID: " + note.getId());
         Toast.makeText(getApplicationContext(), "已保存", Toast.LENGTH_SHORT).show();
-//        cursor.requery();
+
     }
 
     private NoteDao getNoteDao() {
